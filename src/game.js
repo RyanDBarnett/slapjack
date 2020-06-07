6c770573ebe4c6a22551d29546f9ca4554828a28
RyanDBarnett <conner2468@gmail.com>
2020-06-07 09:31:15 -0600

    Create Game class and shuffle method

---
+++ src/game.js
@@ -0,0 +1,33 @@
+class Game {
+	constructor() {
+		this.player1 = new Player(1);
+		this.player2 = new Player(2);
+		this.cards = [1,2,3,4,5,6,7,8,9,10];
+	}
+
+	shuffle(cards) {
+		cards.forEach((card, i) => {
+			const randomIndex = Math.floor(Math.random() * this.cards.length);
+			const temp = card;
+			cards[i] = cards[randomIndex];
+			cards[randomIndex] = temp; 
+		});
+	}
+
+	deal() {
+
+
+	}
+}
+
+// A Game should include:
+// Two Player instances
+// An array of all the possible cards
+// A way to shuffle the deck
+// A way to keep track of the central pile of cards the players will add to
+// A way to deal the deck out to the players
+// A way to keep track of which player’s turn it currently is
+// A way for a player to deal a card into the middle pile
+// A way for players to attempt slapping the pile with varying outcomes (“legal” slaps are Jacks, doubles, and sandwiches - see the playthrough video for further explanation)
+// A way to update a player’s wins count
+// A way to reset the deck and players to play a new game when one is won
